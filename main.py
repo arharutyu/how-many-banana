c1ae14dfcc5b7bb4fea444d67457a6d149c5ebca
arharuty <13937@coderacademy.edu.au>
2023-05-04 17:44:33 +1000

    basic loop in main

---
+++ main.py
@@ -0,0 +1,27 @@
+from bananaconvert import bconvert
+import settings
+
+#Set what the measuring item is & metric/imperial measurements
+item = 'banana'
+measures = 'metric'
+run = True
+
+print('Welcome to How Many Banana?')
+
+#Main loop
+while run == True:
+    print(f"\nYou are measuring with {item}s in the {measures} system.")
+    get_input = input('Type C to convert, S to adjust settings, or Q to quit: ')
+    match get_input:
+        case 'C' 'c':
+            num = input("Enter a number in metres: ")
+            print(f"{num}m is {bconvert(num)} {item}s long!")
+        case 'S':
+            pass
+        case 'Q':
+            print('See you later!')
+            break
+        case _:
+            print("Invalid input")
+
+# with library.csv as item:
