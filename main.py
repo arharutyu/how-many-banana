011c856ccaf278d610b4e32d6e44c838d21e78c8
arharuty <13937@coderacademy.edu.au>
2023-05-04 18:07:24 +1000

    fix match case issue

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ while run == True:
     print(f"\nYou are measuring with {item}s in the {measures} system.")
     get_input = input('Type C to convert, S to adjust settings, or Q to quit: ')
     match get_input:
-        case 'C' 'c':
+        case 'C':
             num = input("Enter a number in metres: ")
             print(f"{num}m is {bconvert(num)} {item}s long!")
         case 'S':
