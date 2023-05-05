e1dc7559e7d28ebc08cc666d7912b495b3c03ea6
arharuty <13937@coderacademy.edu.au>
2023-05-05 23:29:43 +1000

    work on making code more DRY. def settings class

--- bananaconvert.py
+++ bananaconvert.py
@@ -1,9 +1,8 @@
-import csv
+import csv, json
 item_length = 0.13
 
 with open('library.csv') as library:
     reader = csv.DictReader(library, delimiter=',')
-
 
 
 def bconvert(metres):
@@ -24,3 +23,47 @@ def check_library(item):
         for row in reader:
             if item in str(row): 
                 return True
+            
+class Settings:
+    def __init__(self, item, measures):
+        self.item = item
+        self.measures = measures
+#Display current settings
+    def __str__(self, item, measures):
+        return f"\nYou are measuring with {item}s in the {measures} system."
+#Update setting options
+    def update_settings(self, item, measures):
+        run = None
+        update = [{'item': item, 'measures': measures}]
+        while run == None:
+            measures = input('Convert using metric or imperial system?\n')
+            match measures.lower():
+                case 'metric':
+                        update[0]['measures'] = 'metric'
+                        break
+                case 'imperial':
+                        update[0]['measures'] = 'imperial'
+                        break
+                case _:
+                        print("Sorry, please type 'metric' or 'imperial'")
+        while run == None:
+                #Item settings
+                get_item = input('What item would you like to measure with?\n')
+                
+                if check_library(get_item):
+                    update[0]['item'] = str(get_item)
+                    item = get_item
+                    break
+                    # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                    # !!!!!!!!! MAKE SURE YOU ACTUALLY CODE THE CHANGE OF ITEM LENGTH IN BANANACONVERT.PY
+                    # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                else:
+                      print(f"Looks like {get_item} isn't in the library. Would you like to add it's length now? (Yes/No) ")
+                        #Add an item to the library
+
+
+        with open('settings.json', 'w') as s:
+                json.dump(update, s, indent=2)
+                print('Settings have been updated')
+
+        print(self.__str__(item, measures))
--- main.py
+++ main.py
@@ -1,20 +1,24 @@
 from bananaconvert import bconvert, fconvert, check_library
+import bananaconvert
 import json, csv
 
 
 #Set what the measuring item is & metric/imperial measurements
+
 with open('settings.json') as s:
     settings = json.load(s)
     item = str(settings[0]["item"]).lower()
     measures = str(settings[0]["measures"]).lower()
 
+settings = bananaconvert.Settings(item, measures)
+
 #Heading
 print('Welcome to How Many Banana?')
-
+print(settings.__str__(item, measures))
 #Main loop
 run = True
 while run == True:
-    print(f"\nYou are measuring with {item}s in the {measures} system.")
+    
     get_input = input('Type C to convert, S to adjust settings, or Q to quit: ')
     match get_input.lower():
         #Convert to banana feature
@@ -27,41 +31,11 @@ while run == True:
             else:    
                 num = input("Enter a number in metres: ")
                 print(f"{num}m is {bconvert(num)} {item}s long!")
+
         #Adjust settings feature
         case 's':
-            updatesettings = [{'item': item, 'measures': measures}]
-            #Metric/Imperial Settings
-            while run == True:
-                measures = input('Convert using metric or imperial system?\n')
-                match measures.lower():
-                    case 'metric':
-                        updatesettings[0]['measures'] = 'metric'
-                        break
-                    case 'imperial':
-                        updatesettings[0]['measures'] = 'imperial'
-                        break
-                    case _:
-                        print("Sorry, please type 'metric' or 'imperial'")
-            while run == True:
-                #Item settings
-                get_item = input('What item would you like to measure with?\n')
-                
-                if check_library(get_item):
-                    updatesettings[0]['item'] = str(get_item)
-                    item = get_item
-                    break
-                    # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-                    # !!!!!!!!! MAKE SURE YOU ACTUALLY CODE THE CHANGE OF ITEM LENGTH IN BANANACONVERT.PY
-                    # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-                else:
-                      print(f"Looks like {get_item} isn't in the library. Would you like to add it's length now? (Yes/No) ")
-                        #Add an item to the library
-
-
-            #Update settings json file
-            with open('settings.json', 'w') as s:
-                json.dump(updatesettings, s, indent=2)
-                print('Settings have been updated')
+            settings.update_settings(item, measures)
+            settings.__str__(item, measures)
 
         #Quit from menu
         case 'q':
@@ -69,5 +43,3 @@ while run == True:
             break
         case _:
             print("Invalid input")
-
-# with library.csv as item:
