bc8c591a873727edbbb67d20b39d3679295fd48d
arharuty <13937@coderacademy.edu.au>
2023-05-05 18:29:48 +1000

    formatting

--- bananaconvert.py
+++ bananaconvert.py
@@ -1,3 +1,4 @@
+import csv
 item_length = 0.13
 
 def bconvert(metres):
@@ -10,3 +11,11 @@ def fconvert(feet, inches):
     inches = float(inches)
     metres = (feet * 12 + inches) * .0254
     return metres
+
+def check_library(check_item):
+    check_item = check_item
+    with open('library.csv') as library:
+                reader = csv.DictReader(library, delimiter=',')
+                for row in reader:
+                    if check_item not in str(row): 
+                          return False
--- settings.py
+++ settings.py
@@ -1,5 +1,2 @@
-class settings:
-    def __init__(self, item, measures):
-        self.item = item
-        self.measures = measures
 
+    
