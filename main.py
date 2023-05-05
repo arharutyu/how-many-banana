92898d3382f0639cbe5d5f2b7f0225ca5f116cef
arharuty <13937@coderacademy.edu.au>
2023-05-05 15:18:36 +1000

    store settings in json. update measures added

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
 from bananaconvert import bconvert, fconvert
-import settings
+import json
 
 #Set what the measuring item is & metric/imperial measurements
-item = 'banana'
-measures = 'imperial'
-run = True
+with open('settings.json') as s:
+    settings = json.load(s)
+    item = settings[0]["item"]
+    measures = settings[0]["measures"]
 
 #Heading
 print('Welcome to How Many Banana?')
 
 #Main loop
+run = True
 while run == True:
     print(f"\nYou are measuring with {item}s in the {measures} system.")
     get_input = input('Type C to convert, S to adjust settings, or Q to quit: ')
     match get_input:
-        case 'C':
+        case 'C' | 'c':
             if measures == 'imperial':    
                 feet = input("Enter a number in feet: ")
                 inches = input("Enter a number in inches: ")
@@ -23,9 +25,24 @@ while run == True:
             else:    
                 num = input("Enter a number in metres: ")
                 print(f"{num}m is {bconvert(num)} {item}s long!")
-        case 'S':
-            pass
-        case 'Q':
+        case 'S' | 's':
+            updatesettings = [{'item': item, 'measures': measures}]
+            while run == True:
+                measures = input('Convert using metric or imperial system?\n')
+                match measures.lower():
+                    case 'metric':
+                        measures = 'metric'
+                        break
+                    case 'imperial':
+                        measures = 'imperial'
+                        break
+                    case _:
+                        print("Sorry, please type 'metric' or 'imperial'")
+                # item = input('What item would you like to measure with?\n')
+            with open('settings.json', 'w') as s:
+                json.dump(updatesettings, s, indent=2)
+                print('Settings have been updated')
+        case 'Q' | 'q':
             print('See you later!')
             break
         case _:
