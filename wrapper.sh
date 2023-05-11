9aa355df21521627ce63bc158a17b93e39df8ff3
arharuty <13937@coderacademy.edu.au>
2023-05-11 22:33:08 +1000

    auto check for correct python version

--- wrapper.sh
+++ wrapper.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-python3 mainoop.py
+
+pyver="$(command python3 -V)"
+pyverminor="${pyver:9:2}"
+
+if [[ "${pyverminor}" > "09" ]]
+then 
+    python3 mainoop.py
+else 
+    echo "Please install python version 3.10 or higher at https://installpython3.com/" >&2
+    exit 1
+fi
