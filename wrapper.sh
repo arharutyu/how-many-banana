b9b76f005f498322239899ca252cd1adf3e2d303
arharuty <13937@coderacademy.edu.au>
2023-05-12 00:32:15 +1000

    auto activate venv

--- wrapper.sh
+++ wrapper.sh
@@ -5,7 +5,11 @@ pyverminor="${pyver:9:2}"
 
 if [[ "${pyverminor}" > "09" ]]
 then 
+    python3 -m venv .venv
+    source .venv/bin/activate
+    #pip package install here if needed
     python3 mainoop.py
+    deactivate
 else 
     echo "Please install python version 3.10 or higher at https://installpython3.com/" >&2
     exit 1
